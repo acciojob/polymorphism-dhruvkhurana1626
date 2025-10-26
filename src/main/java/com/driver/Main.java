50347f63bced5ded1524cb66b0b12e34e6b0e1ed
dhruv <dhruvkhurana162@gmail.com>
2025-10-27 00:19:29 +0530

    Solution 1

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,10 @@
 package com.driver;
 
 public class Main {
-
+    public static void main(String[] args) {
+        Product p1 = new Product();
+        System.out.println(p1.product(5,4));
+        System.out.println(p1.product(5,4,7));
+        System.out.println(p1.product(5.2,4.5));
+    }
 }
